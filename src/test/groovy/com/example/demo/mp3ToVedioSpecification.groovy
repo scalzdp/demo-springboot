fa4558e238bce58af8b3d626a6deafb23b825ca5
007956@sichuanair.com <007956@sichuanair.com>
2019-04-04 17:48:58 +0800

    更改groovy测试用例

---
+++ src/test/groovy/com/example/demo/mp3ToVedioSpecification.groovy
@@ -0,0 +1,26 @@
+package com.example.demo
+
+import com.example.demo.service.Mp3tovidioService
+import spock.lang.Specification
+
+class mp3ToVedioSpecification extends Specification {
+
+    def mp3tovedio = new Mp3tovidioService();
+
+    def "在图片上增加文字"(){
+
+    }
+
+    def "获取mp3时间长度"(){
+
+    }
+
+    def "通过图片调用ffmpeg生成视频"(){
+
+    }
+
+    def "调用ffmpeg将mp3音频与视频进行合并"(){
+
+    }
+
+}
---
+++ src/test/groovy/com/example/demo/webCrawlerSpecification.groovy
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import spock.lang.Specification
+
+class webCrawlerSpecification extends Specification {
+
+    def "获取待抓取列表中url"(){
+
+    }
+
+    def "通过广度优先算法找出该网站下面复合条件的urls"(){
+
+    }
+
+    def "将符合条件的urls内容爬取到本地"(){
+
+    }
+}
